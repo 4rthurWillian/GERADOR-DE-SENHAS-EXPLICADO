de6eb95299e3836c7d8028e7f67c2ac063b59ee0
4rthurWillian <pohlod.arthur@escola.pr.gov.br>
2026-02-11 11:19:26 -0300

    Caracteres

---
+++ main.js
@@ -0,0 +1,5 @@
+// Definição dos grupos de caracteres disponíveis
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz'; // Corrigi a ordem alfabética aqui
+const numeros = '0123456789';
+const simbolos = '!@%*?';
