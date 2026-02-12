691d79cee9e66d06a6348d1c350703a5e9ef53d8
4rthurWillian <pohlod.arthur@escola.pr.gov.br>
2026-02-12 07:15:56 -0300

    onde usar

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-// Definição dos grupos de caracteres disponíveis
+// Definine os caracteres
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz'; // Corrigi a ordem alfabética aqui
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz'; // Corrigi a ordem alfabética
 const numeros = '0123456789';
 const simbolos = '!@%*?';
+
+// conferir se o que a pessoa digitou está certo; 
+// criar aqueles códigos de brinde e desconto; 
+//  provar que você é um humano e não um robô; 
+//  senhas temporárias de acesso; //
+//  criar nomes de usuário que ninguém mais tem; 
+//  gerar um ponto de partida para sorteios.
